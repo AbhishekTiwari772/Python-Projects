366e90a9bb09f1c81113454314a8f7d7ff41c802
Abhishek Tiwari <84613703+AbhishekTiwari772@users.noreply.github.com>
2021-05-29 12:55:03 +0530

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,15 @@
+#This file explains the full process to declare and use Variables in Python.
+
+"""
+To declare a variable, first give it a name.
+There are some rules to name a variable:
+    1. A variable name can only contain Alphabets, Numbers and Underscore(_).
+    2. A variable name can start with Alphabets and Underscore only.
+    3. No special chareacters other than underscore can be used in a variable name.
+    
+After deciding a name, lets say name_1, you need to put the assignment operator or an equal to sign(=).
+
+After that, you need to give the variable a value. It can be an Integer, Float, String, etc.
+"""
+name_1 = "Abhishek Tiwari" # The variable name_1 will hold the string "Abhishek Tiwari".
+
